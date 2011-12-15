b0bf864e608ee37291ea51176ec4ae82c1e6f744
Dan Johnson <dan.remix@gmail.com>
2011-12-15 15:52:14 -0600

    Renamed media to static so colorfield files are served as static files.  Reformatted code spacings.

--- colorfield/fields.py
+++ colorfield/fields.py
@@ -13,19 +13,20 @@ color_re = re.compile('^([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$')
 validate_color = RegexValidator(color_re, _(u'Enter a valid color.'), 'invalid')
 
 class ColorWidget(forms.Widget):
-    class Media:
-        js = [settings.STATIC_URL + 'colorfield/jscolor.js']
-        
-    def render(self, name, value, attrs=None):
-        return render_to_string('colorfield/color.html', locals())
-        
+  class Media:
+    js = [settings.STATIC_URL + 'colorfield/jscolor.js']
+
+  def render(self, name, value, attrs=None):
+    return render_to_string('colorfield/color.html', locals())
+
+
 class ColorField(models.CharField):
-    default_validators = [validate_color]
+  default_validators = [validate_color]
 
-    def __init__(self, *args, **kwargs):
-        kwargs['max_length'] = 10
-        super(ColorField, self).__init__(*args, **kwargs)
+  def __init__(self, *args, **kwargs):
+    kwargs['max_length'] = 10
+    super(ColorField, self).__init__(*args, **kwargs)
 
-    def formfield(self, **kwargs):
-        kwargs['widget'] = ColorWidget
-        return super(ColorField, self).formfield(**kwargs)
+  def formfield(self, **kwargs):
+    kwargs['widget'] = ColorWidget
+    return super(ColorField, self).formfield(**kwargs)
