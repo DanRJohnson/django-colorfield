48dcff5f3013371f6f4de6e2dffcd5d7160b95a5
Marco Bonetti <mbonetti@gmail.com>
2012-04-01 16:16:14 +0200

    cleanup, ignores

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "django-colorfield",
-    version = "0.0.1",
-    author = "Jared Forsyth, Dan Johnson, Marco Bonetti",
-    author_email = "mbonetti@gmail.com",
-    description = "a small app providing a colorpicker field for django",
-    license = "BSD",
-    keywords = "django color picker",
-    url = "https://github.com/mbi/django-colorfield",
+    name="django-colorfield",
+    version="0.0.2",
+    author="Jared Forsyth, Dan Johnson, Marco Bonetti",
+    author_email="mbonetti@gmail.com",
+    description="a small app providing a colorpicker field for django",
+    license="BSD",
+    keywords="django color picker",
+    url="https://github.com/mbi/django-colorfield",
     packages=find_packages(),
     long_description="",
     classifiers=[
